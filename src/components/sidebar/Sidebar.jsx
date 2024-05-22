0ce199291f98e7f72cf8980ef2dea1daa5ef2517
joramgranger@outlook.com <joramgranger@outlook.com>
2024-05-22 14:46:38 -0700

    boiler plate

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './sidebar.scss'
+
+const Sidebar = () => {
+    return (
+        <div className="sidebar">sidebar</div>
+    )
+}
+
+export default Sidebar;
---
+++ src/pages/list/List.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './list.scss'
+
+const List = () => {
+    return (
+        <div className="list">Home</div>
+    )
+}
+
+export default List;
