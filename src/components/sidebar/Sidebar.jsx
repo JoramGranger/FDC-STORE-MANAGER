abaa7670cc8111d4ed31223eaa237041fddca167
joramgranger@outlook.com <joramgranger@outlook.com>
2024-05-22 16:25:08 -0700

    sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,9 +1,92 @@
 import React from "react";
 import './sidebar.scss'
 
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import PeopleOutlineIcon from '@mui/icons-material/PeopleOutline';
+import ShoppingBagOutlinedIcon from '@mui/icons-material/ShoppingBagOutlined';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import LocalShippingOutlinedIcon from '@mui/icons-material/LocalShippingOutlined';
+import BarChartOutlinedIcon from '@mui/icons-material/BarChartOutlined';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import MonitorHeartOutlinedIcon from '@mui/icons-material/MonitorHeartOutlined';
+import DvrOutlinedIcon from '@mui/icons-material/DvrOutlined';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
+
 const Sidebar = () => {
     return (
-        <div className="sidebar">sidebar</div>
+        <div className="sidebar">
+
+            {/* Top */}
+            <div className="top">
+                <span className="logo">Fortune Derma</span>
+            </div>
+            <hr/>
+            {/* Center */}
+            <div className="center">
+                <ul>
+                    <p className="title">MAIN</p>
+                    <li>
+                        <DashboardIcon className="icon"/>
+                        <span>Dashboard</span>
+                    </li>
+                    <p className="title">LISTS</p>
+                    <li>
+                        <PeopleOutlineIcon className="icon"/>
+                        <span>Users</span>
+                    </li>
+                    <li>
+                        <ShoppingBagOutlinedIcon className="icon"/>
+                        <span>Products</span>
+                    </li>
+                    <li>
+                        <ShoppingCartOutlinedIcon className="icon"/>
+                        <span>Orders</span>
+                    </li>
+                    <li>
+                        <LocalShippingOutlinedIcon className="icon"/>
+                        <span>Delivery</span>
+                    </li>
+                    <p className="title">USEFUL</p>
+                    <li>
+                        <BarChartOutlinedIcon className="icon"/>
+                        <span>Stats</span>
+                    </li>
+                    <li>
+                        <NotificationsNoneOutlinedIcon className="icon"/>
+                        <span>Notification</span>
+                    </li>
+                    <p className="title">SYSTEM</p>
+                    <li>
+                        <MonitorHeartOutlinedIcon className="icon"/>
+                        <span>System Health</span>
+                    </li>
+                    <li>
+                        <DvrOutlinedIcon className="icon"/>
+                        <span>Logs</span>
+                    </li>
+                    <li>
+                        <SettingsOutlinedIcon className="icon"/>
+                        <span>Settings</span>
+                    </li>
+                    <p className="title">USER</p>
+                    <li>
+                        <AccountCircleOutlinedIcon className="icon"/>
+                        <span>Profile</span>
+                    </li>
+                    <li>
+                        <LogoutOutlinedIcon className="icon"/>
+                        <span>Logout</span>
+                    </li>
+                </ul>
+            </div>
+
+            {/* Bottom */}
+            <div className="bottom">
+                color options
+            </div>
+        </div>
     )
 }
 
